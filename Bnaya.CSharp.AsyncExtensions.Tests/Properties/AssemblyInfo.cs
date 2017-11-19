da5e80ca2a80c73c4dca41413056ce6eee038673
bnaya eshet <bnayanet@hotmail.com>
2017-11-19 09:14:15 +0200

    seed

---
+++ Bnaya.CSharp.AsyncExtensions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bnaya.CSharp.AsyncExtensions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bnaya.CSharp.AsyncExtensions.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49fac379-9b9d-41e8-9b00-a9c9f7165a12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bnaya.CSharp.AsyncExtensions.Tests/WithTimeout_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Bnaya.CSharp.AsyncExtensions.Tests
+{
+    [TestClass]
+    public class WithTimeout_Tests
+    {
+        [TestMethod]
+        public async Task WithTimeout_Pass()
+        {
+            await Task.Delay(1)
+                      .WithTimeout(TimeSpan.FromMilliseconds(100))
+                      .ConfigureAwait(false);
+        }
+    }
+}
